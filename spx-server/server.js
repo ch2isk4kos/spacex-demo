a3fc05e28c32d767f915529758c9db1af344264b
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-20 17:36:19 -0400

    add route for graphql

--- spx-server/server.js
+++ spx-server/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const graphqlHTTP = ("express-graphql");
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
+app.use("/graphql", graphqlHTTP({
+  schema,
+  graphql: true  
+}));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`);
