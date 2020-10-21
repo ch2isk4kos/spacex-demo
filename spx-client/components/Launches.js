a1d83830e3bd1ca46cf0714d911e7acf5602c4a2
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-21 17:46:18 -0400

    refactor to class component and import graphql dependencies

--- spx-client/components/Launches.js
+++ spx-client/components/Launches.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
-const Launches = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const LAUNCHES_QUERY = gql`
+  query LauncesQuery {
+    launches: {
+      flight_number
+      mission_name
+      launch_date_local
+      launch_success
+    }
+  }
+`
+
+export class Launches extends Component {
+  render() {
+    return (
+      <div>
+        <h1 classNName="display-4 my-3">Launches</h1>
+        <Query query={LAUNCHES_QUERY}>
+          {
+            ({ loading, error, data }) => {
+              if (loading) return <h4>Loading...</h4>   // @TODO: add an animated loader
+              if (error) console.log(error)             // @TODO: add error message for user
+              console.log("data: ", data)
+              
+              return <h1>testing</h1>
+            }
+          }
+        </Query>
+      </div>
+    )
+  }
 }
 
 export default Launches;
