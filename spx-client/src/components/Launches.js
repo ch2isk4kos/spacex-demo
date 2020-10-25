8fc9a71ff595c9ca1d73cd2a3bce8e49237dac21
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-25 02:55:08 -0400

    refactor to functional component and fix query name syntax error

--- spx-client/src/components/Launches.js
+++ spx-client/src/components/Launches.js
@@ -1,41 +1,97 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import Launch from './Launch';
 
 const LAUNCHES_QUERY = gql`
-  query LauncesQuery {
-    launches: {
+  query LaunchesQuery {
+    launches {
       flight_number
       mission_name
       launch_date_local
       launch_success
     }
   }
-`
+`;
 
-class Launches extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    const { loading, error, data } = useQuery(LAUNCHES_QUERY);
-    
-    if (loading) return <h3>Loading...</h3>;                              // @TODO: add an animated loader
-    if (error) return <h3>Something Went Wrong: {error.message}</h3>;     // @TODO: add error message for user
-    console.log("data: ", data)
-
-    return (
-      <div>
-        <h1 classNName="display-4 my-3">Launches</h1>
+const Launches = () => {
+  const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+  if (loading) return <h3>Loading...</h3>;                              // @TODO: add an animated loader
+  if (error) return <h3>Something Went Wrong: {error.message}</h3>;     // @TODO: add error message for user
+  console.log("data: ", data);
+  return (
+    <div>
+      <h1 classNName="display-4 my-3">Launches</h1>
         {
           data.launches.map(launch => {
-             <Launch key={flight_launch_number} launch={launch} />
+             return <Launch key={launch.flight_number} launch={launch} />
           })
         }
-      </div>
-    )
-  }
+    </div>
+  )
 }
+
+// class Launches extends Component {
+//   render() {
+//     const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+//     if (loading) return <h3>Loading...</h3>;                              // @TODO: add an animated loader
+//     if (error) return <h3>Something Went Wrong: {error.message}</h3>;     // @TODO: add error message for user
+//     console.log("data: ", data);
+
+//     return (data && data.launches ? (
+//       <div>
+//         <h1 classNName="display-4 my-3">Launches</h1>
+//          {
+//            data.launches.map(launch => {
+//               return <Launch key={launch.flight_number} launch={launch} />
+//            })
+//          }
+//       </div>
+//     ) : (
+//       <div>
+//         <h3>Loading</h3>
+//       </div>
+//     ));
+//   }
+// }
+
+// class Launches extends Component {
+//   render() {
+//     const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+//     if (loading) return <h3>Loading...</h3>;                              // @TODO: add an animated loader
+//     if (error) return <h3>Something Went Wrong: {error.message}</h3>;     // @TODO: add error message for user
+//     console.log("data: ", data);
+
+//     return (
+//       <>
+//         <h1 classNName="display-4 my-3">Launches</h1>
+//         {
+//           data.launches.map(launch => {
+//              <Launch key={launch.flight_number} launch={launch} />
+//           })
+//         }
+//       </>
+//     )
+//   }
+// }
+
+// class Launches extends Component {
+//   render() {
+//     const { loading, error, data } = useQuery(LAUNCHES_QUERY);
+//     if (loading) return <h3>Loading...</h3>;                              // @TODO: add an animated loader
+//     if (error) return <h3>Something Went Wrong: {error.message}</h3>;     // @TODO: add error message for user
+//     console.log("data: ", data);
+//     return (
+//       <div>
+//         <h1 classNName="display-4 my-3">Launches</h1>
+//         {
+//           data.launches.map(launch => {
+//              <Launch key={launch.flight_number} launch={launch} />
+//           })
+//         }
+//       </div>
+//     )
+//   }
+// }
 
 // class Launches extends Component {
 //   render() {
@@ -48,7 +104,6 @@ class Launches extends Component {
 //               if (loading) return <h4>Loading...</h4>   // @TODO: add an animated loader
 //               if (error) console.log(error)             // @TODO: add error message for user
 //               console.log("data: ", data)
-
 //               // return <h1>testing</h1>
 //               return (
 //                 <div>
