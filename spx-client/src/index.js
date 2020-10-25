7897a38a3b9d9004564e1ecb733b18334472d901
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-24 23:35:31 -0400

    import and wrap ApolloProvder around root component

--- spx-client/src/index.js
+++ spx-client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { ApolloClient, InMemooryCache } from '@apollo/client';
+import { ApolloClient, InMemooryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: `http://localhost:3001/graphql`,
@@ -12,7 +12,9 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
