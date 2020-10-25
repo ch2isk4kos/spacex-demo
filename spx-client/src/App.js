ab38841415a72ac36d0a2477942d27fef8cb9007
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-25 01:18:33 -0400

    import launches component and add to render

--- spx-client/src/App.js
+++ spx-client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import logo from './spacex-logo.png';
+import Launches from './components/Launches';
 
 function App() {
   return (
     <div className="main">
-      <img src={logo} alt="SpaceX" style={{ display: "block", margin: "auto" }}/>
+      <img src={logo} alt="SpaceX" style={{ display: "block", margin: "auto" }} />
+      <Launches />
     </div>
   );
 }
