0a55e4f374c8d30c2d214130318dea41fd9bc5d2
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-25 02:52:44 -0400

    remove console.log and comment out code

--- spx-client/src/components/Launch.js
+++ spx-client/src/components/Launch.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
+// const Launch = (props) => {
+//   console.log(props.launch);
+//   const { flight_launch_number, mission_name, launch_date_local, launch_success } = props.launch;
+//   return (
+//     <div className="card card-body mb-3">
+//       <div className="row">
+//         <div className="col-md-9">
+//           <h4>{mission_name}</h4>
+//           <p>{launch_date_local}</p>
+//         </div>
+//         <div className="col-md-3">
+//           <button className="btn btn-secondary">Launch Details</button>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
 const Launch = (props) => {
-  console.log(props.launch);
-  const { flight_number, mission_name, launch_date_local, launch_success } = props.launch
+  const { flight_number, mission_name, launch_date_local, launch_success } = props.launch;
   return (
     <div className="card card-body mb-3">
       <div className="row">
