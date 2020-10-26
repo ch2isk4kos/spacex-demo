d78a2dcf2094d50f855b28f90c52feda24a37ea0
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-25 20:26:01 -0400

    import react-router-dom components and render routing for launches

--- spx-client/src/App.js
+++ spx-client/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import logo from './spacex-logo.png';
 import Launches from './components/Launches';
+import { Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="main">
-      <img src={logo} alt="SpaceX" style={{ display: "block", margin: "auto" }} />
-      <Launches />
+    <div className="App">
+      <img src={logo} alt="SpaceX" style={{ width: "300", display: "block", margin: "auto" }} />
+      {/* <Launches /> */}
+      
+      <Switch>
+        <Route exact path={"/"} component={Launches} />
+        <Route exact path={"/launch/:flight_number"} component={""} />
+      </Switch>
     </div>
   );
 }
