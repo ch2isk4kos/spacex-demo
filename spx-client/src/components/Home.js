ce1b6e71a5ce4b49679dc59d604884ee5a768499
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-26 18:16:06 -0400

    remove link

--- spx-client/src/components/Home.js
+++ spx-client/src/components/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <Link to={`/launches`} className="btn btn-secondary">Launches</Link>
     </div>
   )
 }
