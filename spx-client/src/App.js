8d7c2b1af505c5a19cd6491121712646e4d24afc
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-26 18:05:17 -0400

    import home component and add route

--- spx-client/src/App.js
+++ spx-client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import logo from './spacex-logo.png';
+// import logo from './spacex-logo.png';
+import Home from './components/Home';
 import Launches from './components/Launches';
 import Launch from './components/Launch';
 import { Switch, Route } from 'react-router-dom';
@@ -9,12 +10,13 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <img src={logo} alt="SpaceX" style={{ width: "300", display: "block", margin: "auto" }} />
+      
       {/* <Launches /> */}
       
       <Switch>
-        <Route exact path={"/"} component={Launches} />
-        <Route exact path={"/launch/:flight_number"} component={Launch} />
+        <Route exact path={"/"} component={Home} />
+        <Route exact path={"/launches"} component={Launches} />
+        <Route exact path={"/launches/:flight_number"} component={Launch} />
       </Switch>
     </div>
   );
