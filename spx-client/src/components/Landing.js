ca12b50e1052791c04f4afffc1348b6e537bbfeb
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-26 23:51:06 -0400

    import and add background video for landing page

--- spx-client/src/components/Landing.js
+++ spx-client/src/components/Landing.js
@@ -1,13 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import globe from '../Earth.mp4';
+import globe from '../hd0924.mov';
+// import globe from '../Starfield_Fly_Through.mov';
+// import globe from '../506401051_1.mov';
+import '../styles/Landing.css';
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
-      <Link to={`/home`} className="btn btn-secondary">Enter</Link>
-    </div>
-  )
+    <header className="Landing">
+      <video
+        autoPlay
+        muted
+        loop
+        style={{
+          position: "fixed",
+          width: "100%",
+          left: 0,
+          top: 0,
+          // opacity: this.state.loading ? 0 : 1,
+          transition: "opacity, 2s ease-in-out"
+        }}
+      >
+        <source src={globe} type="video/mp4" />
+      </video>
+    </header>
+  );
 }
 
 export default Landing;
