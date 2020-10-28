dac2bf2b3775a63509846e0d7f687edd45a0728a
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-27 23:31:44 -0400

    refactor landing component structure

--- spx-client/src/components/Landing.js
+++ spx-client/src/components/Landing.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 // import globe from '../Earth.mp4';
 import logo from '../spacex-logo-2.png';
+// import logo from '../spacex-logo-4.png';
 import globe from '../hd0924.mov';
-// import logo from '../spacex-logo-2.png';
+// import logo from '../spacex-logo-3.png';
 // import globe from '../Starfield_Fly_Through.mov';
 // import globe from '../506401051_1.mov';
 import '../styles/Landing.css';
@@ -20,41 +21,13 @@ const Landing = () => {
       </div>
       
       <div className="content">
-        <h1>VIDEO ON BACKGROUND</h1>
         <img src={logo} alt="SpaceX" />
+        <h1>To Advance the Future</h1>
         <p>The <em>object-fit</em> CSS property specifies how the contents of a replaced element should be fitted to the box established by its used height and width (from <a href="https://developer.mozilla.org/pt-BR/docs/Web/CSS/object-fit">MDN</a>).</p>
         <Link to={"/home"} className="btn btn-success link">Enter</Link>
       </div>
     </section>
   );
 }
-
-// const Landing = () => {
-//   return (
-//     <section className="Landing">
-//       <div className="video-bg-wrap">
-//         <video
-//           autoPlay
-//           muted
-//           loop
-//           style={{
-//             position: "fixed",
-//             width: "100%",
-//             height: "100%",
-//             left: 0,
-//             top: 0,
-//             transition: "opacity, 2s ease-in-out"
-//           }}>
-//           <source src={globe} type="video/mp4" />
-//           <div className="LandingOverlay"></div>
-//           <div className="LandingContent">
-//             <img src={logo} alt="SpaceX" />
-//             <span className="link"><Link to={"/home"} className="btn btn-success">Enter</Link></span>
-//           </div>
-//         </video>
-//       </div>
-//     </section>
-//   );
-// }
 
 export default Landing;
