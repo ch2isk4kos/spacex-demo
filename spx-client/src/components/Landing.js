88ec9ca825c0ea153e3a7ef457eb00d0d126a661
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-29 23:46:42 -0400

    test different background options

--- spx-client/src/components/Landing.js
+++ spx-client/src/components/Landing.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-// import globe from '../Earth.mp4';
-import logo from '../spacex-logo-2.png';
+// import logo from '../spacex-logo-2.png';
+import symbol from '../spacex-logo-3.png';
 // import logo from '../spacex-logo-4.png';
+// import logo from '../spacex-logo-5.png';
+import logo from '../spacex-logo-6.png';
+// import logo from '../spacex-logo-7.png';
 import globe from '../hd0924.mov';
-// import logo from '../spacex-logo-3.png';
+// import globe from '../hd0576.mov';
+// import globe from '../Earth.mp4';
+// import globe from '../Earth to Moon Flight.mov';
 // import globe from '../Starfield_Fly_Through.mov';
-// import globe from '../506401051_1.mov';
 import '../styles/Landing.css';
 
 const Landing = () => {
@@ -22,9 +26,9 @@ const Landing = () => {
       
       <div className="content">
         <img src={logo} alt="SpaceX" />
-        <h1>To Advance the Future</h1>
+        <h1 className="mission-statment">to advance the future</h1>
         <p>The <em>object-fit</em> CSS property specifies how the contents of a replaced element should be fitted to the box established by its used height and width (from <a href="https://developer.mozilla.org/pt-BR/docs/Web/CSS/object-fit">MDN</a>).</p>
-        <Link to={"/home"} className="btn btn-success link">Enter</Link>
+        <Link to={"/home"} className="btn btn-success link">Launch</Link>
       </div>
     </section>
   );
