2be4a89f9b547e85d46ff6f8b489f0a9b116ad69
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-05 01:53:51 -0500

    import and render home components

--- spx-client/src/components/home/Home.js
+++ spx-client/src/components/home/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import Left from './Left';
+import RightTop from './RightTop';
+import RightBottom from './RightBottom';
+import '../../styles/Home.css'
 
 const Home = () => {
   return (
     <div className="Home">
-      <h1>Home</h1>
       <div className="home-left">
+        <Left />
       </div>
       <div className="home-right">
+        <div className="right-top">
+          <RightTop />
+        </div>
+        <div className="right-bottom"> 
+          <RightBottom />
+        </div>
       </div>
     </div>
   )
