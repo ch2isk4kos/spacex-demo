0b7c5d0e87794e278597626401e51338da28db8c
ch2isk4kos <ch2isk4kos@gmail.com>
2020-11-05 00:25:25 -0500

    create directory for home components

---
+++ spx-client/src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>Home</h1>
+      <div className="home-left">
+      </div>
+      <div className="home-right">
+      </div>
+    </div>
+  )
+}
+
+export default Home;
